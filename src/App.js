483bb2689c7bc2a266f0e4ba3f1e28fea2bc2c1b
前端小菜鸟 <718781500@qq.com>
2019-11-20 09:55:38 +0800

    配置好了移动适配,antd,开启了装饰器,开启了模块化css语法

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Carouseler from './Carousel';
-
+import AppCss from "./App.module.css"
 
 function App() {
   return (
     <div>
+      <h1 className={AppCss.hello} id={AppCss.font}>hello 1907</h1>
       <Carouseler />
     </div>
   );
